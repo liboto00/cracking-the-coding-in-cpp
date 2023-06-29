514f865163e3c52f0e18f5c1896da16f70a56b93
wblee <wblee800@gmail.com>
2023-06-29 20:41:24 +0900

    Add 10811.cpp

---
+++ cracking-the-coding-in-c++/10811.cpp
@@ -0,0 +1,17 @@
+// 백준 온라인 저지 10811번 https://www.acmicpc.net/problem/10811
+// 제한 시간 : 1초
+// 실행 시간 : ms
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	std::vector<int> vec;
+
+		
+}
