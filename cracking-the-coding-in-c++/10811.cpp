82161a32b30dd223c4361c89f20f51c738ac9262
wblee <wblee800@gmail.com>
2023-06-29 22:19:41 +0900

    Edit 10811.cpp

--- cracking-the-coding-in-c++/10811.cpp
+++ cracking-the-coding-in-c++/10811.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 int main()
 {
@@ -12,6 +13,31 @@ int main()
 	std::cout.tie(NULL);
 
 	std::vector<int> vec;
+	int N, M;
+	int startNumber, endNumber;
 
-		
+	std::cin >> N >> M;
+
+	for (int i = 0; i < N; ++i)
+	{
+		vec.push_back(i + 1);
+	}
+
+	std::vector<int>::iterator itr = vec.begin();
+	for (int i = 0; i < M; i++)
+	{
+		std::cin >> startNumber >> endNumber;
+		startNumber -= 1;
+		endNumber -= 1;
+
+		for (int j = startNumber; j <= endNumber; j++)
+		{
+			reverse(vec.begin() + startNumber, vec.begin() + endNumber);
+		}
+	}
+
+	for (const auto& i : vec)
+	{
+		std::cout << i << " ";
+	}
 }
