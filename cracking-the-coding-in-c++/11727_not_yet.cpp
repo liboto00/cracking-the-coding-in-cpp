cb5a2af56cc18815496e2daad3e07954e49e52de
wblee <wblee800@gmail.com>
2023-08-16 12:24:13 +0900

    Create 11727_not_yet

---
+++ cracking-the-coding-in-c++/11727_not_yet.cpp
@@ -0,0 +1,17 @@
+// 백준 온라인 저지 11727번 https://www.acmicpc.net/problem/11727
+// 제한 시간 : 1초
+// 실행 시간 : ms
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int arr[1001] = { 1, 3, 5 };
+
+	int n;
+}
