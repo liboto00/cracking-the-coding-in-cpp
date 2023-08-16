9011627d30076d5bffca9cf4159472c195644c3a
wblee <wblee800@gmail.com>
2023-08-16 12:25:14 +0900

    Create 2156_not_yet

---
+++ cracking-the-coding-in-c++/2156_not_yet.cpp
@@ -0,0 +1,15 @@
+// 백준 온라인 저지 2156번 https://www.acmicpc.net/problem/2156
+// 제한 시간 : 2초
+// 실행 시간 : ms
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	
+}
