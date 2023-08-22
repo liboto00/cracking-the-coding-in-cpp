74c9d1a866f3ca5f1be8293fc7d07a92c83a390b
Wonbin Lee <89712521+wblee800@users.noreply.github.com>
2023-08-22 09:45:20 +0900

    Update and rename 11727_not_yet.cpp to 11727.cpp

--- cracking-the-coding-in-c++/dynamic-programming/11727.cpp
+++ cracking-the-coding-in-c++/dynamic-programming/11727.cpp
@@ -1,8 +1,8 @@
 // 백준 온라인 저지 11727번 https://www.acmicpc.net/problem/11727
 // 제한 시간 : 1초
-// 실행 시간 : ms
+// 실행 시간 : 0ms
 
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main()
@@ -10,8 +10,16 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
-	int arr[1001] = { 1, 3, 5 };
-
+	
 	int n;
+	cin >> n;
+
+	int arr[1001] = { 0, 1, 3};
+
+	for (int i = 3; i < 1001; i++)
+	{
+		arr[i] = (arr[i - 1] + 2 * arr[i - 2]) % 10007;
+	}
+
+	cout << arr[n];
 }
