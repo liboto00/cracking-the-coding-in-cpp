aa1ce34119da6cd795cd31cb4775b371d0808159
wonbinlee00@gmail.com <526473+gb96@wblee800.github.com>
2023-09-02 12:26:36 +0900

    add .

---
+++ cracking-the-coding-in-c++/bfs-and-dfs/1697_not_yet.cpp
@@ -0,0 +1,21 @@
+// 백준 온라인 저지 1697번 https://www.acmicpc.net/problem/1697
+// 제한 시간 : 2초
+// 실행 시간 : ms
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, K;
+	cin >> N >> K;
+
+	vector<int> visited(100000, 0);
+	
+	
+}
--- cracking-the-coding-in-c++/dynamic-programming/2156_not_yet.cpp
+++ cracking-the-coding-in-c++/dynamic-programming/2156_not_yet.cpp
@@ -3,6 +3,7 @@
 // 실행 시간 : ms
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
@@ -11,5 +12,23 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	int n;
+	cin >> n;
+
+	vector<int> theNumOfWine(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> theNumOfWine[i];
+	}
 	
+	// 마지막 와인을 마실 수 있는 경우와 없는 경우
+	vector<int> wine00(2, 0); // o
+	vector<int> wine01(2, 0); // o
+	vector<int> wine10(2, 0); // o
+	vector<int> wine11(2, 0); // x
+
+	for (int i = 0; i < n; i++)
+	{
+		
+	}
 }
