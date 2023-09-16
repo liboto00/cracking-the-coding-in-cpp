be045116af6467658f37527416b7be89cb141c82
wonbinlee00@gmail.com <526473+gb96@wblee800.github.com>
2023-09-16 17:24:43 +0900

    Edit .

--- cracking-the-coding-in-c++/data-structure/priority-queue-and-heap/14235_not_yet.cpp
+++ cracking-the-coding-in-c++/data-structure/priority-queue-and-heap/14235_not_yet.cpp
@@ -12,8 +12,8 @@ int main()
     int n; // 거점지 방문 횟수(1≤n≤5,000)
     int a; // 충전할 선물 개수(1≤a≤100)
 
-	int gift; // 선물들의 가치
-	vector<int> gifts; // 선물들의 가치를 담는 배열
+	int gift; // 선물들의 가치(0≤gift<100,000)
+	vector<int> gifts; // 선물들의 가치를 담는 배열의 최대 크기 n(5,000) * a(100)
 
 	cin >> n;
 	for (int i = 0; i < n; i++)
@@ -28,9 +28,6 @@ int main()
 			}
 			else
 			{
-				// gifts를 오름차순으로 정렬
-				sort(gifts.begin(), gifts.end());
-				
 				// 끝에 있는 가장 큰 수 출력
 				cout << *(gifts.end() - 1) << '\n';
 
@@ -48,6 +45,9 @@ int main()
 				// gifts에 대입
 				gifts.push_back(gift);
 			}
+
+			// gifts를 오름차순으로 정렬
+				sort(gifts.begin(), gifts.end());
 		}
 	}
 }
