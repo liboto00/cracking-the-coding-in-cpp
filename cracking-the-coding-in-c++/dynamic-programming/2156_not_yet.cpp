958404e9db7eb47a0a2478244038dc56da62c27e
wonbinlee00@gmail.com <526473+gb96@wblee800.github.com>
2023-09-16 14:07:32 +0900

    .

--- cracking-the-coding-in-c++/dynamic-programming/2156_not_yet.cpp
+++ cracking-the-coding-in-c++/dynamic-programming/2156_not_yet.cpp
@@ -20,26 +20,15 @@ int main()
 	{
 		cin >> theNumOfWine[i];
 	}
-	
+
 	// 마지막 와인을 마실 수 있는 경우와 없는 경우
 	vector<int> wine00(2, 0); // o
 	vector<int> wine01(2, 0); // o
 	vector<int> wine10(2, 0); // o
 	vector<int> wine11(2, 0); // x
 
-	int maxSizeofWine;
-	for (int i = 2; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		wine01[1] = theNumOfWine[i];
-		wine10[0] = theNumOfWine[i];
-		wine11[0] = theNumOfWine[i];
-		wine11[1] = theNumOfWine[i];
-
-		maxSizeofWine = max(wine00[0] + wine00[1] + theNumOfWine[i], 
-							wine01[0] + wine01[1] + theNumOfWine[i],
-							wine10[0] + wine10[1] + theNumOfWine[i]);
-		maxSizeofWine = max(maxSizeofWine, wine11[0] + wine11[1]);
+		
 	}
-
-	cout << maxSizeofWine;
 }
