5e6fa278dd8fe01ab09f62b959db9cef2aa8e3e7
Wonbin Lee <wblee800@gmail.com>
2023-10-15 12:55:23 +0900

    Edit 1149.cpp

--- cracking-the-coding-in-c++/1149_not_yet.cpp
+++ cracking-the-coding-in-c++/1149_not_yet.cpp
@@ -6,6 +6,46 @@
 #include<vector>
 using namespace std;
 
+int N;
+vector<vector<int>> colorOfStreet(N, vector<int>(3, 0));
+int visited[3][3] = {};
+int path[3] = {};
+int cnt = 0;
+int minNum = 1001;
+void Recursive(int level)
+{
+	if (level == N)
+	{
+		if (path[0] + path[1] + path[2] < minNum)
+		{
+			minNum = path[0] + path[1] + path[2];
+		}
+
+		return;
+	}
+
+	for (int y = 0; y < 3; y++)
+	{
+		for (int x = 0; x < 3; x++)
+		{
+			if (y == 0 && visited[y][x] == 0)
+			{
+				path[level] = colorOfStreet[y][x];
+				Recursive(level + 1);
+				path[level] = 0;
+				break;
+			}
+			else if (y > 0 && visited[y - 1][x] == 0)
+			{
+				path[level] = colorOfStreet[y][x];
+				Recursive(level + 1);
+				path[level] = 0;
+				break;
+			}
+		}
+	}
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -13,25 +53,17 @@ int main()
 	cout.tie(NULL);
 
 	// 집의 수 N(2 ≤ N ≤ 1,000)
-	int N;
 	cin >> N;
 
-	vector<vector<int>> colorOfStreet(N, vector<int>(3));
-	for (size_t y = 0; y < N; y++)
+	for (int y = 0; y < N; y++)
 	{
-		for (size_t x = 0; x < 3; x++)
+		for (int x = 0; x < 3; x++)
 		{
 			cin >> colorOfStreet[y][x];
 		}
 	}
 
 	// 인접한 집과 같은 색이면 안 된다는 조건을 적용해 Brute force
-	// e.g.
-	// 첫 번째 행의 첫 번째 열의 색을 선택한 경우, 두 번째 행은 첫 번째 열을 선택하지 못한다.
-	
-	// 4, 4, 4번 실행
-	for (size_t i = 0; i < ; i++)
-	{
-
-	}
+	// e.g. 첫 번째 행의 첫 번째 열의 색을 선택한 경우, 두 번째 행은 첫 번째 열을 선택하지 못한다.
+	Recursive(0);
 }
