4474a402e4e38fb8edcfdf252f3180096f3a345c
Wonbin Lee <wblee800@gmail.com>
2023-10-26 18:18:12 +0900

    .

--- cracking-the-coding-in-c++/bfs-and-dfs/1697_not_yet.cpp
+++ cracking-the-coding-in-c++/bfs-and-dfs/1697_not_yet.cpp
@@ -16,18 +16,44 @@ int main()
 	int N, K;
 	cin >> N >> K;
 
+	if (N == K)
+	{
+		cout << 0;
+
+		return 0;
+	}
+
 	// 수빈이의 위치가 X일 때, 1초 후, 이동 위치
 	// 1. X - 1
 	// 2. X + 1
 	// 3. 2 * X 이동
 	vector<int> position(2 * K, 0);
 	int nowN = N;
-	int direction[3] = {nowN - 1, nowN + 1, 2 * nowN};
+	int direction[4] = {nowN - 1, nowN + 1, 2 * nowN};
+	int cntStep = 0;
+
 	while (!position[K])
 	{
-		if (position[nowN] == 0)
-			position[nowN] = 1;
-		
-		// dfs로 풀기
+		cntStep++;
+
+		for (size_t i = 0; i < 3; i++)
+		{
+			nowN = direction[i];
+
+			if ((0 <= nowN && nowN <= 100000)
+				&& position[nowN] == 0)
+			{
+				position[nowN] = cntStep;
+
+				if (nowN == K)
+				{
+					cout << position[K];
+					
+					return 0;
+				}
+			}
+		}
 	}
+
+	cout << position[K];
 }
