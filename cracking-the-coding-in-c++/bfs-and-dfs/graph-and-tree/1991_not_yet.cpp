2f14cfdcb0613cd5827aa6233c5e787a1938564e
Wonbin Lee <wblee800@gmail.com>
2023-11-02 13:53:51 +0900

    Edit 1991_not_yet.xpp

--- cracking-the-coding-in-c++/bfs-and-dfs/graph-and-tree/1991_not_yet.cpp
+++ cracking-the-coding-in-c++/bfs-and-dfs/graph-and-tree/1991_not_yet.cpp
@@ -4,8 +4,30 @@
 
 #include <iostream>
 #include <vector>
-#include <tuple>
 using namespace std;
+
+int N;
+string path;
+void SearchBinaryTree(vector<char>& tree, int level)
+{
+	// 말단 노드까지 탐색하면,
+	if ()
+	{
+		// 탐색한 노드를 출력 후,
+		cout << path;
+
+		// 반환
+		return;
+	}
+
+	path.push_back(tree[level]);
+	SearchBinaryTree(tree, 2 * level);
+	path.pop_back();
+
+	path.push_back(tree[level + 1]);
+	SearchBinaryTree(tree, 2 * level + 1);
+	path.pop_back();
+}
 
 int main()
 {
@@ -13,45 +35,59 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int N;
 	cin >> N;
 
-	typedef tuple<char, char, char> tup;
-	vector<tup> binary_tree;
+	char nodes[4] = {};
+	vector<char> tree(N * 3 + 1, '.');
 
-	// vector와 tuple을 사용해 각 노드와 왼쪽 자식 노드, 오른쪽 자식 노드 입력
-	char node, left_child, right_child;
-	for (int i = 0; i < N; i++)
+	// 노드(i), 왼쪽 자식 노드(2 * i), 오른쪽 자식 노드 입력(2 * i + 1)
+	int indexRange = 1;
+	for (size_t i = 1; i <= N; i++)
 	{
-		cin >> node >> left_child >> right_child;
-
-		if (left_child == '.')
+		cin >> nodes[0];
+		int indexOfParent = -1;
+		if (tree[i] == '.')
 		{
-			left_child = 0;
+			bool isExist = false;
+			for (size_t j = i; j < indexRange; j++)
+			{
+				// 입력한 값이 트리에 이미 있으면,
+				if (nodes[0] == tree[j])
+				{
+					isExist = true;
+					indexOfParent = j;
+
+					break;
+				}
+			}
+
+			if (!isExist)
+			{
+				tree[i] = nodes[0];
+				indexOfParent = i;
+			}
 		}
-		if (right_child == '.')
+		else
 		{
-			right_child = 0;
+			indexOfParent = i;
 		}
 
-		binary_tree.push_back(make_tuple(node, left_child, right_child));
+		// i 기준이 아니라, 첫 번째로 입력받은 부모 노드가 있는 인덱스 기준으로 설정해야 함.
+		cin >> nodes[1];
+		if (tree[2 * indexOfParent] == '.')
+		{
+			tree[2 * indexOfParent] = nodes[1];
+		}
+		
+		cin >> nodes[2];
+		if (tree[2 * indexOfParent + 1] == '.')
+		{
+			tree[2 * indexOfParent + 1] = nodes[2];
+		}
+
+		// 현재까지 입력된 노드들이 존재하는 범위
+		indexRange = 2 * i + 1;
 	}
 
-	// 전위 순회(루트) (왼쪽 자식) (오른쪽 자식)
-	for (size_t i = 0; i < length; i++)
-	{
-
-	}
-
-	// 중위 순회(왼쪽 자식) (루트) (오른쪽 자식)
-	for (size_t i = 0; i < length; i++)
-	{
-
-	}
-
-	// 후위 순회(왼쪽 자식) (오른쪽 자식) (루트)
-	for (size_t i = 0; i < length; i++)
-	{
-
-	}
+	SearchBinaryTree(tree, 1);
 }
