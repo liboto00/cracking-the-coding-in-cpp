0c84c60ff5f174c1eab5051fe3346f171fd688cf
Wonbin Lee <wblee800@gmail.com>
2023-11-04 20:25:07 +0900

    Edit 1149_not_yet.cpp

--- cracking-the-coding-in-c++/dynamic-programming/1149_not_yet.cpp
+++ cracking-the-coding-in-c++/dynamic-programming/1149_not_yet.cpp
@@ -31,104 +31,159 @@ int main()
 
 	if (colorOfHouses[0][0] + colorOfHouses[1][1])
 	{
-		minOfCost = colorOfHouses[0][0] + colorOfHouses[1][1];
 		minOfXY[0].first = 0;
 		minOfXY[0].second = 0;
 		minOfXY[1].first = 1;
 		minOfXY[1].second = 1;
 	}
+
+	minOfCost = colorOfHouses[minOfXY[0].first][minOfXY[0].second] +
+		colorOfHouses[minOfXY[1].first][minOfXY[1].second];
 
 	if (colorOfHouses[0][0] + colorOfHouses[1][2] < minOfCost)
 	{
-		minOfCost = colorOfHouses[0][0] + colorOfHouses[1][2];
 		minOfXY[0].first = 0;
 		minOfXY[0].second = 0;
 		minOfXY[1].first = 1;
 		minOfXY[1].second = 2;
 	}
 
+	minOfCost = colorOfHouses[minOfXY[0].first][minOfXY[0].second] +
+		colorOfHouses[minOfXY[1].first][minOfXY[1].second];
+
 	if (colorOfHouses[0][1] + colorOfHouses[1][0] < minOfCost)
 	{
-		minOfCost = colorOfHouses[0][1] + colorOfHouses[1][0];
 		minOfXY[0].first = 0;
 		minOfXY[0].second = 1;
 		minOfXY[1].first = 1;
 		minOfXY[1].second = 0;
 	}
 
+	minOfCost = colorOfHouses[minOfXY[0].first][minOfXY[0].second] +
+		colorOfHouses[minOfXY[1].first][minOfXY[1].second];
+
 	if (colorOfHouses[0][1] + colorOfHouses[1][2] < minOfCost)
 	{
-		minOfCost = colorOfHouses[0][1] + colorOfHouses[1][2];
 		minOfXY[0].first = 0;
 		minOfXY[0].second = 1;
 		minOfXY[1].first = 1;
 		minOfXY[1].second = 2;
 	}
 
+	minOfCost = colorOfHouses[minOfXY[0].first][minOfXY[0].second] +
+		colorOfHouses[minOfXY[1].first][minOfXY[1].second];
+
 	if (colorOfHouses[0][2] + colorOfHouses[1][0] < minOfCost)
 	{
-		minOfCost = colorOfHouses[0][2] + colorOfHouses[1][0];
 		minOfXY[0].first = 0;
 		minOfXY[0].second = 2;
 		minOfXY[1].first = 1;
 		minOfXY[1].second = 0;
 	}
 
+	minOfCost = colorOfHouses[minOfXY[0].first][minOfXY[0].second] +
+		colorOfHouses[minOfXY[1].first][minOfXY[1].second];
+
 	if (colorOfHouses[0][2] + colorOfHouses[1][1] < minOfCost)
 	{
-		minOfCost = colorOfHouses[0][2] + colorOfHouses[1][1];
 		minOfXY[0].first = 0;
 		minOfXY[0].second = 2;
 		minOfXY[1].first = 1;
 		minOfXY[1].second = 1;
 	}
+
+	minOfCost = colorOfHouses[minOfXY[0].first][minOfXY[0].second] +
+		colorOfHouses[minOfXY[1].first][minOfXY[1].second];
 
 	selected[minOfXY[0].first][minOfXY[0].second] = 1;
 	selected[minOfXY[1].first][minOfXY[1].second] = 1;
 
-	for (size_t y = 2; y < N; y++)
+	int tempMinOfCost = 1001;
+	for (size_t y = 2; y < N - 1; y++)
 	{
 		for (size_t x = 0; x < 3; x++)
 		{
-			if (selected[y - 2][x] == 1)
+			if (selected[y - 1][x] == 1)
 			{
+				tempMinOfCost = 1001;
+
 				switch (x)
 				{
 				case 0:
-					if (colorOfHouses[y][1] < colorOfHouses[y][2])
+					if (colorOfHouses[y][1] + colorOfHouses[y + 1][2] < minOfCost)
 					{
-						minOfCost += colorOfHouses[y][1];
-						selected[y][1] = 1;
+						minOfXY[0].first = y;
+						minOfXY[0].second = 1;
+						minOfXY[1].first = y + 1;
+						minOfXY[1].second = 2;
 					}
-					else
+
+					tempMinOfCost = colorOfHouses[minOfXY[0].first][minOfXY[0].second] +
+						colorOfHouses[minOfXY[1].first][minOfXY[1].second];
+
+					if (colorOfHouses[y][2] + colorOfHouses[y + 1][1] < tempMinOfCost)
 					{
-						minOfCost += colorOfHouses[y][2];
-						selected[y][2] = 1;
+						minOfXY[0].first = y;
+						minOfXY[0].second = 2;
+						minOfXY[1].first = y + 1;
+						minOfXY[1].second = 1;
 					}
+
+					minOfCost += colorOfHouses[minOfXY[0].first][minOfXY[0].second] +
+						colorOfHouses[minOfXY[1].first][minOfXY[1].second];
+
+					selected[y][minOfXY[0].second] = 1;
+
 					break;
 				case 1:
-					if (colorOfHouses[y][0] < colorOfHouses[y][2])
+					if (colorOfHouses[y][0] + colorOfHouses[y + 1][2] < minOfCost)
 					{
-						minOfCost += colorOfHouses[y][0];
-						selected[y][0] = 1;
+						minOfXY[0].first = y;
+						minOfXY[0].second = 0;
+						minOfXY[1].first = y + 1;
+						minOfXY[1].second = 2;
 					}
-					else
+
+					tempMinOfCost = colorOfHouses[minOfXY[0].first][minOfXY[0].second] +
+						colorOfHouses[minOfXY[1].first][minOfXY[1].second];
+
+					if (colorOfHouses[y][2] + colorOfHouses[y + 1][0] < tempMinOfCost)
 					{
-						minOfCost += colorOfHouses[y][2];
-						selected[y][2] = 1;
+						minOfXY[0].first = y;
+						minOfXY[0].second = 2;
+						minOfXY[1].first = y + 1;
+						minOfXY[1].second = 0;
 					}
+
+					minOfCost += colorOfHouses[minOfXY[0].first][minOfXY[0].second] +
+						colorOfHouses[minOfXY[1].first][minOfXY[1].second];
+
+					selected[y][minOfXY[0].second] = 1;
 					break;
 				case 2:
-					if (colorOfHouses[y][0] < colorOfHouses[y][1])
+					if (colorOfHouses[y][0] + colorOfHouses[y + 1][1] < minOfCost)
 					{
-						minOfCost += colorOfHouses[y][0];
-						selected[y][0] = 1;
+						minOfXY[0].first = y;
+						minOfXY[0].second = 0;
+						minOfXY[1].first = y + 1;
+						minOfXY[1].second = 1;
 					}
-					else
+
+					tempMinOfCost = colorOfHouses[minOfXY[0].first][minOfXY[0].second] +
+						colorOfHouses[minOfXY[1].first][minOfXY[1].second];
+
+					if (colorOfHouses[y][1] + colorOfHouses[y + 1][0] < tempMinOfCost)
 					{
-						minOfCost += colorOfHouses[y][1];
-						selected[y][1] = 1;
+						minOfXY[0].first = y;
+						minOfXY[0].second = 1;
+						minOfXY[1].first = y + 1;
+						minOfXY[1].second = 0;
 					}
+
+					minOfCost += colorOfHouses[minOfXY[0].first][minOfXY[0].second] +
+						colorOfHouses[minOfXY[1].first][minOfXY[1].second];
+
+					selected[y][minOfXY[0].second] = 1;
 					break;
 				default:
 					break;
