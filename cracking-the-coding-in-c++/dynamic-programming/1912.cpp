5e4cfe4b93ee5555d162b1b42ef25ea8d5f99e37
Wonbin Lee <wblee800@gmail.com>
2023-11-19 16:51:32 +0900

    Solve a 1912.cpp

---
+++ cracking-the-coding-in-c++/dynamic-programming/1912.cpp
@@ -0,0 +1,43 @@
+// 백준 온라인 저지 1912번 https://www.acmicpc.net/problem/1912
+// 제한 시간 : 1초
+// 실행 시간 : ms
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<int> sequence(n, 0);
+	for (size_t i = 0; i < n; i++)
+	{
+		cin >> sequence[i];
+	}
+
+	for (size_t i = 1; i < n; i++)
+	{
+		int temp = sequence[i - 1] + sequence[i];
+
+		if (sequence[i] < temp)
+		{
+			sequence[i] = temp;
+		}
+	}
+
+	int maxSum = -1001;
+	for (size_t i = 0; i < n; i++)
+	{
+		maxSum = max(maxSum, sequence[i]);
+	}
+
+	cout << maxSum;
+
+	return 0;
+}
